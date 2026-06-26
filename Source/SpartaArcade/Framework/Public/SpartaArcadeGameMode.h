0070d54b7ceadb97d3c8d46df0cdb7230cf6637a
Jungbin Choi <j1b2c3@naver.com>
2026-06-26 10:34:34 +0900

    git init

---
+++ Source/SpartaArcade/Framework/Public/SpartaArcadeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpartaArcadeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASpartaArcadeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASpartaArcadeGameMode();
+};
+
+
+
---
+++ Source/SpartaArcade/SpartaArcade.Build.cs
@@ -0,0 +1,37 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+
+public class SpartaArcade : ModuleRules
+{
+	public SpartaArcade(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+        
+        PublicIncludePaths.AddRange(new string[]
+        {
+	        ModuleDirectory,
+	        Path.Combine(ModuleDirectory, "Characters/Public"),
+	        Path.Combine(ModuleDirectory, "Framework/Public"),
+	        Path.Combine(ModuleDirectory, "Items/Public"),
+	        Path.Combine(ModuleDirectory, "Level/Public"),
+	        Path.Combine(ModuleDirectory, "Spawn/Public"),
+	        Path.Combine(ModuleDirectory, "Systems/Public"),
+	        Path.Combine(ModuleDirectory, "UI/Public")
+        });
+
+        PrivateIncludePaths.AddRange(new string[]
+        {
+	        Path.Combine(ModuleDirectory, "Characters/Private"),
+	        Path.Combine(ModuleDirectory, "Framework/Private"),
+	        Path.Combine(ModuleDirectory, "Items/Private"),
+	        Path.Combine(ModuleDirectory, "Level/Private"),
+	        Path.Combine(ModuleDirectory, "Spawn/Private"),
+	        Path.Combine(ModuleDirectory, "Systems/Private"),
+	        Path.Combine(ModuleDirectory, "UI/Private")
+        });
+    }
+}
