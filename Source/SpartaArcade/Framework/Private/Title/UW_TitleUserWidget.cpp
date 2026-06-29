f052e93f7f78161ca2f25848754cc4cda06849e4
Cha0910 <ae20414@naver.com>
2026-06-29 19:36:03 +0900

    feat: 임시 타이틀 및 서버 접속 구현 - 서버 IP 입력 및 접속을 위한 임시 타이틀 위젯, 플레이어 컨트롤러 추가 - 기본 시작 맵 변경 - ASpartaArcadeController InputMode 변경 추가

---
+++ Source/SpartaArcade/Framework/Private/Title/UW_TitleUserWidget.cpp
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Title/UW_TitleUserWidget.h"
+#include "Components/Button.h"
+#include "Components/EditableText.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Title/TitlePlayerController.h"
+
+UUW_TitleUserWidget::UUW_TitleUserWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UUW_TitleUserWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	if (IsValid(PlayButton) == true)
+	{
+		PlayButton->OnClicked.AddDynamic(this, &UUW_TitleUserWidget::OnPlayButtonClicked);
+	}
+	if (IsValid(ExitButton) == true)
+	{
+		ExitButton->OnClicked.AddDynamic(this, &UUW_TitleUserWidget::OnExitButtonClicked);
+	}
+}
+
+void UUW_TitleUserWidget::OnPlayButtonClicked()
+{
+	ATitlePlayerController* TitlePlayerController = Cast<ATitlePlayerController>(GetOwningPlayer());
+	if(IsValid(TitlePlayerController) == true)
+	{
+		if (IsValid(ServerIPEditableText) == true)
+		{
+			FString ServerIP = ServerIPEditableText->GetText().ToString();
+			if (ServerIP.IsEmpty() == false)
+			{
+				FString PlayerName;
+				if (IsValid(PlayerNameEditableText) == true)
+				{
+					PlayerName = PlayerNameEditableText->GetText().ToString();
+				}
+				//TitlePlayerController->PlayerState->SetPlayerName(PlayerName);
+				TitlePlayerController->JoinServer(ServerIP);
+			}
+		}
+	}
+}
+
+void UUW_TitleUserWidget::OnExitButtonClicked()
+{
+	UKismetSystemLibrary::QuitGame(this, GetOwningPlayer(), EQuitPreference::Quit, false);
+}
