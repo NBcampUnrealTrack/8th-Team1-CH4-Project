95ec795f08c31fe2d6bedea5ec20bd4ccfaf9b2b
osh4306 <osh4306@naver.com>
2026-07-14 19:33:36 +0900

    feat(System) : 실드아이템 변경 완료 및 UI업데이트

--- Source/SpartaArcade/Characters/Public/SpartaArcadeCharacter.h
+++ Source/SpartaArcade/Characters/Public/SpartaArcadeCharacter.h
@@ -68,12 +68,16 @@ public:
 	void UseFirstAidKit();
 	
 	UFUNCTION(BlueprintCallable, Category = "Gameplay")
+	void UseShield();
+	
+	UFUNCTION(BlueprintCallable, Category = "Gameplay")
 	void KickBomb();
 
 	// 아이템 획득 및 쿨다운 처리를 위한 상태 함수 추가
 	void AddFirstAidKit();
 	void AddShield();
 	void OnBombExploded();
+	void PerformUseShield();
 	void UnlockKickBomb();
 
 	// UI 및 HUD 연동을 위한 Getter 함수
@@ -125,6 +129,9 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Abilities")
 	TSubclassOf<UGameplayAbility> KickBombAbilityClass;
+	
+	UPROPERTY(EditdefaultsOnly, BlueprintReadOnly, Category = "Abilities")
+	TSubclassOf<UGameplayAbility> UseShieldAbilityClass;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Abilities")
 	TSubclassOf<UGameplayAbility> UseFirstAidKitAbilityClass;
