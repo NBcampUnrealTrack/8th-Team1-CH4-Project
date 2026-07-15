b45604a6ada353a889ce6c251697882ab2048a94
Jungbin Choi <j1b2c3@naver.com>
2026-07-15 14:36:10 +0900

    1. SpartaArcadeCharacter.h EditDefaultsOnly 오타 수정
    2. AddMappingContext 중볼 호출 정리
    
    3. SpartaArcadeCharacter.cpp 위젯 호출 버그 수정
    
    4. DebugMessage 주석 처리
    
    5. 미니맵을 위한 캡처 Tick 컴포넌트 최적화
    
    6. 디버그 로그 정리 (Warning -> Verbose)
    
    7. 미니맵 각 플레이어 별 정상 구동하도록 수정
    
    8. 스폰 위치 조정, 스폰 위치에 안겹치도록 맵 생성 로직 수정, 장애물에 스폰킬 당하지 않도록 스폰 지점에 장애물이 접근하지 못하도록 수정

--- Source/SpartaArcade/Level/Public/SpartaArcadeMapGenerator.h
+++ Source/SpartaArcade/Level/Public/SpartaArcadeMapGenerator.h
@@ -13,6 +13,14 @@ class SPARTAARCADE_API ASpartaArcadeMapGenerator : public AActor
 public:	
 	ASpartaArcadeMapGenerator();
 
+	// 맵 생성 절차가 이미 수행 완료되었는지 기록하는 플래그 (선제 맵 빌드 타이밍 제어용)
+	UPROPERTY(Transient, BlueprintReadOnly, Category = "Map Generation")
+	bool bMapGenerated = false;
+
+	// 기획 규칙에 따라 절차적으로 맵을 생성 (게임모드가 선제 완료할 수 있도록 public 공개)
+	UFUNCTION(BlueprintCallable, Category = "Map Generation")
+	void GenerateMap();
+
 protected:
 	virtual void BeginPlay() override;
 
@@ -39,10 +47,11 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Map Settings", meta = (ClampMin = "0.0", ClampMax = "1.0"))
 	float BlockSpawnChance;
 
-	// 기획 규칙에 따라 절차적으로 맵을 생성
-	UFUNCTION(BlueprintCallable, Category = "Map Generation")
-	void GenerateMap();
-
 	// 격자 좌표가 플레이어 스폰 안전 구역(4개 모서리 및 인접 3개 타일)인지 판별하는 헬퍼 함수
 	bool IsSafeZone(int32 X, int32 Y) const;
+
+	void RepositionPlayerStarts();
+
+	// Modified: 스폰 포인트 주변의 구조물/장애물 액터 강제 파괴 함수 추가
+	void ClearStructuresAtSpawns();
 };
