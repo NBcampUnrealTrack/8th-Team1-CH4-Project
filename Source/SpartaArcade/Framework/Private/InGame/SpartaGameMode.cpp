44c48facddd6a9ac5b55a2476891565e359f3b48
Cha0910 <ae20414@naver.com>
2026-07-20 23:06:52 +0900

    fix: 결과 UI 리더보드 중복 수정 - HasMatchEnded() 조건을 추가해 중복 실행 방지 - 게임 결과 로직 종료 후 Super::EndMatch()가 실행되도록 함수 하단으로 이동

--- Source/SpartaArcade/Framework/Private/InGame/SpartaGameMode.cpp
+++ Source/SpartaArcade/Framework/Private/InGame/SpartaGameMode.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "InGame/SpartaGameMode.h"
@@ -116,11 +116,9 @@ void ASpartaGameMode::StartMatch()
 
 void ASpartaGameMode::EndMatch()
 {
-	Super::EndMatch();
-
 	UE_LOG(LogTemp, Warning, TEXT("Match Ended!"));
 
-	if (!IsValid(SpartaGameState))
+	if (!IsValid(SpartaGameState) || HasMatchEnded())
 	{
 		return;
 	}
@@ -144,6 +142,8 @@ void ASpartaGameMode::EndMatch()
 	}
 	Algo::Reverse(MatchResults);
 	ShowGameResultToAllPlayers();
+
+	Super::EndMatch();
 }
 
 void ASpartaGameMode::HandlePlayerEliminated(ASpartaPlayerState* DeadPlayer, ASpartaPlayerState* KillerPlayerState, EDeathReason Reason)
